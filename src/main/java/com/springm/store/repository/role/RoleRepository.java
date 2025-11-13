599f05d53b08bae6768f89e056f9c64f85e87f38
Andrii Honchar <andriihoncharw@gmail.com>
2025-11-13 23:05:40 +0200

    added SecurityConfig

---
+++ src/main/java/com/springm/store/repository/role/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.springm.store.repository.role;
+
+import com.springm.store.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
