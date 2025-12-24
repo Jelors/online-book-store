76c918da357fd824dc9efa245994cf2143d52a3d
Andrii Honchar <andriihoncharw@gmail.com>
2025-12-24 12:15:31 +0200

    added validation for Status dto

--- src/main/java/com/springm/store/dto/order/UpdateOrderStatusDto.java
+++ src/main/java/com/springm/store/dto/order/UpdateOrderStatusDto.java
@@ -1,11 +1,13 @@
 package com.springm.store.dto.order;
 
 import com.springm.store.model.Order;
+import com.springm.store.validation.order.Status;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateOrderStatusDto {
+    @Status(enumClass = Order.Status.class)
     private Order.Status status;
 }
