ae26a47c04699c69f0c940ee29382c018b07932b
Andrii Honchar <andriihoncharw@gmail.com>
2026-01-07 12:11:10 +0200

    all tests for BookService

---
+++ src/test/java/com/springm/store/repository/BookRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.springm.store.repository;
+
+import com.springm.store.model.Book;
+import com.springm.store.model.Category;
+import com.springm.store.repository.book.BookRepository;
+import com.springm.store.repository.category.CategoryRepository;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("Find all books that belongs to category with ID: 1")
+    void findAllByCategoryId_EqualsOne_ReturnsListWithOneBook() {
+        Category category = new Category();
+        category.setName("Fantastic");
+        category.setDescription("Fantastic books");
+        categoryRepository.save(category);
+
+        Book book = new Book();
+        book.setTitle("Harry Potter");
+        book.setAuthor("J. K. Rowling");
+        book.setPrice(BigDecimal.TEN);
+        book.setIsbn("978-0547928213");
+        book.setDescription("Such a good book about a boy that survived");
+        book.setCoverImage("harryPotter.png");
+        book.setCategories(Set.of(category));
+        bookRepository.save(book);
+
+        List<Book> actual = bookRepository.findAllByCategories_Id(1L);
+
+        Assertions.assertEquals(1, actual.size());
+    }
+
+    @Test
+    @DisplayName("Returns empty list, when tries to find by non-existing category.")
+    void findAllByCategoryId_EqualsNonExistingId_ReturnsEmptyList() {
+        Book book = new Book();
+        book.setTitle("Harry Potter");
+        book.setAuthor("J. K. Rowling");
+        book.setPrice(BigDecimal.TEN);
+        book.setIsbn("978-0547928213");
+        book.setDescription("Such a good book about a boy that survived");
+        book.setCoverImage("harryPotter.png");
+        book.setCategories(Set.of());
+        bookRepository.save(book);
+
+        List<Book> actual = bookRepository.findAllByCategories_Id(2L);
+
+        Assertions.assertEquals(0, actual.size());
+    }
+
+}
---
+++ src/test/java/com/springm/store/service/BookServiceTest.java
@@ -0,0 +1,190 @@
+package com.springm.store.service;
+
+import com.springm.store.dto.book.BookDto;
+import com.springm.store.dto.book.CreateBookRequestDto;
+import com.springm.store.mapper.BookMapper;
+import com.springm.store.model.Book;
+import com.springm.store.model.Category;
+import com.springm.store.repository.book.BookRepository;
+import com.springm.store.repository.book.BookSpecificationBuilder;
+import com.springm.store.repository.category.CategoryRepository;
+import com.springm.store.service.impl.BookServiceImpl;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookServiceTest {
+    @Autowired
+    private BookServiceImpl bookService;
+
+    @MockitoBean
+    private BookRepository bookRepository;
+
+    @MockitoBean
+    private CategoryRepository categoryRepository;
+
+    @MockitoBean
+    private BookMapper bookMapper;
+
+    @MockitoBean
+    private BookSpecificationBuilder bookSpecificationBuilder;
+
+    @Test
+    @DisplayName("Save book with valid input")
+    void save_ValidInput_Success() {
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("Poetry");
+
+        Mockito.when(categoryRepository.findById(1L))
+                .thenReturn(Optional.of(category));
+
+        CreateBookRequestDto requestDto = new CreateBookRequestDto();
+        requestDto.setTitle("Kobzar");
+        requestDto.setAuthor("Taras Shevchenko");
+        requestDto.setPrice(BigDecimal.valueOf(24));
+        requestDto.setCategoryIds(Set.of(1L));
+
+        Book bookEntity = new Book();
+        bookEntity.setTitle("Kobzar");
+        bookEntity.setAuthor("Taras Shevchenko");
+        bookEntity.setPrice(BigDecimal.valueOf(24));
+
+        BookDto bookDto = new BookDto();
+        bookDto.setTitle("Kobzar");
+        bookDto.setAuthor("Taras Shevchenko");
+        bookDto.setPrice(BigDecimal.valueOf(24));
+        bookDto.setCategoryIds(Set.of(1L));
+
+        Mockito.when(bookMapper.toModel(Mockito.any(CreateBookRequestDto.class)))
+                .thenReturn(bookEntity);
+
+        Mockito.when(bookRepository.save(Mockito.any(Book.class)))
+                .thenReturn(bookEntity);
+
+        Mockito.when(bookMapper.toDto(Mockito.any(Book.class)))
+                .thenReturn(bookDto);
+        BookDto actual = bookService.save(requestDto);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals("Kobzar", actual.getTitle());
+        Assertions.assertEquals("Taras Shevchenko", actual.getAuthor());
+        Assertions.assertEquals(BigDecimal.valueOf(24), actual.getPrice());
+
+    }
+
+    @Test
+    @DisplayName("Find book by id, returns BookDto")
+    void findById_ValidInput_Success() {
+
+        Book bookEntity = new Book();
+        bookEntity.setId(1L);
+        bookEntity.setTitle("Kobzar");
+        bookEntity.setAuthor("Taras Shevchenko");
+
+        BookDto bookDto = new BookDto();
+        bookDto.setId(1L);
+        bookDto.setTitle("Kobzar");
+        bookDto.setAuthor("Taras Shevchenko");
+
+        Mockito.when(bookRepository.findById(1L))
+                .thenReturn(Optional.of(bookEntity));
+
+        Mockito.when(bookMapper.toDto(bookEntity))
+                .thenReturn(bookDto);
+
+        BookDto actual = bookService.findById(1L);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(1L, actual.getId());
+        Assertions.assertEquals("Kobzar", actual.getTitle());
+        Assertions.assertEquals("Taras Shevchenko", actual.getAuthor());
+    }
+
+    @Test
+    @DisplayName("Find all books")
+    void findAll_Pageable_ReturnsAllBooks() {
+        Pageable pageable = PageRequest.of(0, 1);
+
+        Book book = new Book();
+        book.setId(1L);
+
+        Page<Book> bookPage =
+                new PageImpl<>(List.of(book), pageable, 2);
+
+        Mockito.when(bookRepository.findAll(pageable))
+                .thenReturn(bookPage);
+
+        Mockito.when(bookMapper.toDto(book))
+                .thenReturn(new BookDto());
+
+        Page<BookDto> result = bookService.findAll(pageable);
+
+        Assertions.assertEquals(1, result.getContent().size());
+        Assertions.assertEquals(2, result.getTotalElements());
+    }
+
+    @Test
+    @DisplayName("Updates book with valid id and input dto")
+    void updateBookById_ValidInput_Success() {
+        Book bookEntity = new Book();
+        bookEntity.setId(1L);
+        bookEntity.setTitle("Harry Potter");
+        bookEntity.setAuthor("J. K. Rowling");
+
+
+        Mockito.when(bookRepository.findById(1L))
+                .thenReturn(Optional.of(bookEntity));
+
+        CreateBookRequestDto changedBookDto = new CreateBookRequestDto();
+        changedBookDto.setTitle("The World of Ice and Fire");
+        changedBookDto.setAuthor("George R. R. Martin");
+
+
+        BookDto bookDto = new BookDto();
+        bookDto.setId(1L);
+        bookDto.setTitle("The World of Ice and Fire");
+        bookDto.setAuthor("George R. R. Martin");
+
+
+        Mockito.when(bookMapper.toDto(bookEntity))
+                .thenReturn(bookDto);
+
+        BookDto actual = bookService.updateBookById(1L, changedBookDto);
+
+        Assertions.assertEquals(1L, actual.getId());
+        Assertions.assertEquals("The World of Ice and Fire", actual.getTitle());
+        Assertions.assertEquals("George R. R. Martin", actual.getAuthor());
+    }
+
+    @Test
+    @DisplayName("Deletes books with valid id")
+    void deleteBookById_ValidInput_Success() {
+        Mockito.doNothing()
+                .when(bookRepository)
+                .deleteById(1L);
+
+        bookService.deleteBookById(1L);
+
+        Mockito.verify(bookRepository)
+                .deleteById(1L);
+    }
+
+}
